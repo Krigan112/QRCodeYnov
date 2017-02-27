52434e9d4ed9fbe61fbaf8c8200ab3e5562889a7
yann9602 <yann.avondo@gmail.com>
2017-02-27 15:26:30 +0100

    QR Code work

--- app/src/main/java/qr/qrynov/MainActivity.java
+++ app/src/main/java/qr/qrynov/MainActivity.java
@@ -1,33 +1,31 @@
 package qr.qrynov;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+// on importe les classes IntentIntegrator et IntentResult de la librairie zxing
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private IntentIntegrator qrScan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
+        FloatingActionButton mybutton = (FloatingActionButton) findViewById(R.id.fab);
+        mybutton.setOnClickListener(this);
+        }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,5 +46,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+        public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+            IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+            if (scanningResult != null) {
+                String scanContent = scanningResult.getContents();
+                String scanFormat = scanningResult.getFormatName();
+
+                TextView scan_format = (TextView) findViewById(R.id.TypeQRCode);
+                TextView scan_content = (TextView) findViewById(R.id.ContentQRCode);
+
+                scan_format.setText("FORMAT: " + scanFormat);
+                scan_content.setText("CONTENT: " + scanContent);
+            }
+            else{
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Aucune donnée reçu!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+        }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.fab){
+            new IntentIntegrator(this).initiateScan();
+        }
+
     }
 }
