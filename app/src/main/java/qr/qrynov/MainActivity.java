22183e763b2ebd079ff9f251510ad617bed371a3
yann9602 <yann.avondo@gmail.com>
2017-02-28 10:47:16 +0100

    Qr code create

--- app/src/main/java/qr/qrynov/MainActivity.java
+++ app/src/main/java/qr/qrynov/MainActivity.java
@@ -1,6 +1,8 @@
 package qr.qrynov;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +10,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.google.zxing.qrcode.QRCodeWriter;
@@ -20,6 +25,7 @@ import com.google.zxing.qrcode.QRCodeWriter;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private IntentIntegrator qrScan;
+    private BitMatrix mybitmatrix;
     QRCodeWriter myQrCodeCreate = new QRCodeWriter();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +66,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String scanFormat = scanningResult.getFormatName();
                 TextView scan_format = (TextView) findViewById(R.id.TypeQRCode);
                 TextView scan_content = (TextView) findViewById(R.id.ContentQRCode);
+                int width = 150;
+                int height = 150;
+                try {
+                    mybitmatrix=  myQrCodeCreate.encode(scanContent,BarcodeFormat.QR_CODE,width,height);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+                Bitmap ImageBitmap = Bitmap.createBitmap(width,
+                        height, Bitmap.Config.ARGB_8888);
+
+                for (int i = 0; i < width; i++) {// width
+                    for (int j = 0; j < height; j++) {// height
+                        ImageBitmap.setPixel(i, j, mybitmatrix.get(i, j) ? Color.BLACK
+                                : Color.WHITE);
+                    }
+                }
+                ImageView QrCodeImageView = (ImageView) findViewById(R.id.imageViewQrcode);
+                QrCodeImageView.setImageBitmap(ImageBitmap);
+
+
+
+
 
                 scan_format.setText("FORMAT: " + scanFormat);
                 scan_content.setText("CONTENT: " + scanContent);
-                myQrCodeCreate.encode(scanContent, BarcodeFormat.QR_CODE)
+
             }
             else{
                 Toast toast = Toast.makeText(getApplicationContext(),
@@ -73,13 +101,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
 
-
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.fab){
            IntentIntegrator myintent =  new IntentIntegrator(this);
-            myintent.setOrientationLocked(false);
             myintent.initiateScan();
+            myintent.setOrientationLocked(false);
         }
 
     }
